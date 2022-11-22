6780d8305158e6ed433f707f7b82a869b775fb7e
Harvey Gleeson <harveygleeson@gmail.com>
2022-11-22 16:54:51 +0000

    initial commit

---
+++ TweeterTags/ImageTVCell.swift
@@ -0,0 +1,12 @@
+//
+//  imageTVCell.swift
+//  TweeterTags
+//
+//  Created by Harvey on 21/11/2022.
+//
+
+import UIKit
+
+class ImageTVCell: UITableViewCell {    
+    @IBOutlet weak var tweetImage: UIImageView!
+}
---
+++ TweeterTags/MentionsTVCell.swift
@@ -0,0 +1,12 @@
+//
+//  mentionsTVCell.swift
+//  TweeterTags
+//
+//  Created by Harvey on 21/11/2022.
+//
+
+import UIKit
+
+class MentionsTVCell: UITableViewCell {
+    @IBOutlet weak var mentionText: UILabel!
+}
